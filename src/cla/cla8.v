ca1fba75a5a66ce49447d415e3a16c97035c4a87
annmaria00410-art <annmaria00410@gmail.com>
2026-08-28 23:44:57 +0530

    Update cla8.v

--- src/cla/cla8.v
+++ src/cla/cla8.v
@@ -1,1 +1,50 @@
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02.07.2026 01:53:29
+// Design Name: 
+// Module Name: cla8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cla8(
+    input [7:0] A,
+    input [7:0] B,
+    input Cin,
+    output [7:0] Sum,
+    output Cout
+);
+
+wire C4;
+
+cla4 LOWER(
+    .A(A[3:0]),
+    .B(B[3:0]),
+    .Cin(Cin),
+    .Sum(Sum[3:0]),
+    .Cout(C4)
+);
+
+cla4 UPPER(
+    .A(A[7:4]),
+    .B(B[7:4]),
+    .Cin(C4),
+    .Sum(Sum[7:4]),
+    .Cout(Cout)
+);
+
+endmodule
